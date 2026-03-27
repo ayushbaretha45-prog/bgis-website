1ab03e219fccb275085748980a2f2612ced5b0c4
ayushbaretha45-prog <ayushbaretha45@gmail.com>
2026-03-27 10:17:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -87,5 +87,6 @@ app.post("/update", checkAuth, (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
