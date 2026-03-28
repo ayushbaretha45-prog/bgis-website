2a788ee0a185090bfddf84ab916e87e68f90b57e
ayush <ayushbaretha45@gmail.com>
2026-03-28 10:11:23 +0530

    removed seedteams auto insert

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const teamSchema = new mongoose.Schema({
 });
 
 const Team = mongoose.model("Team", teamSchema);
-async function seedTeams() {
+async function  {
   const count = await Team.countDocuments();
   if (count === 0) {
     await Team.insertMany([
